eff5c5ac7d004111cb482e2ac6de03136e3f9889
gahyeon lee <22gahyeon@naver.com>
2022-04-07 02:05:41 +0900

    first commit

---
+++ 4x4Main.cpp
@@ -0,0 +1,10 @@
+//4x4 ∆€¡Ò ∞‘¿”
+#include "Ranking.h"
+extern int playFifteenPuzzle();
+void main()
+{
+	loadRanking("ranking.txt");
+	int rank = playFifteenPuzzle();
+	printRanking();
+	storeRanking("ranking.txt");
+}
